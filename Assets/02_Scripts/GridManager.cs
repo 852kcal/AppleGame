002eb0e5b51295744dd8eacfded5142e0f5f1594
852kcal <inseong6629@gmail.com>
2026-02-10 20:10:18 +0900

    사과 배열 작업 수정
    10*7 => 18*10 으로 수정 변수로 위치값 잡도록 수정
    
    에셋 스토어에서 프루츠 에셋 추가

--- Assets/02_Scripts/GridManager.cs
+++ Assets/02_Scripts/GridManager.cs
@@ -7,17 +7,23 @@ public class GridManager : MonoBehaviour
     public GameObject applePrefab;
     public GameObject gameField;
 
+    public int rows = 10;
+    public int columns = 18;
+
+    private float cellWidth = 0.7f;
+    private float cellHeight = 0.7f;
+
     // Start is called before the first frame update
     void Start()
     {
-        for(int i=0;i<10;i++)
+        for(int i=0;i<columns;i++)
         {
-            for(int j=0;j<7;j++)
+            for(int j=0;j<rows;j++)
             {
-                Instantiate(applePrefab,new Vector3(i,j,0), Quaternion.identity, gameField.transform);
+                Instantiate(applePrefab,new Vector3(i*cellWidth,j*cellHeight,0), Quaternion.identity, gameField.transform);
             }
         }
-        gameField.transform.position = new Vector3(-4.75f, -3f, 0);
+        gameField.transform.position = new Vector3(-(columns-1)*cellWidth/2f, -(rows-1)*cellHeight/2f, 0);
     }
 
     // Update is called once per frame
