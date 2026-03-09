ecd097e39bd9b5b6efb5a592ce855c8f1582a135
jimin100606 <inseong6629@gmail.com>
2026-03-09 19:50:19 +0900

    메인화면 UI 수정중
    폴리싱이 잘 되지않아서 계속 수정중

---
+++ Assets/02_Scripts/ButtonAnimation.cs
@@ -0,0 +1,18 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonAnimation : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale = Vector3.one * 1.05f;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = Vector3.one;
+    }
+}
